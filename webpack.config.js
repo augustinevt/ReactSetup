3da93c95e99b795f0c25557c084e8964fe5a5874
Augustine  von Trapp <augustinevontrapp@Augustines-MacBook-Pro.local>
2017-02-23 22:53:16 -0800

    basic setup functional

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './source/react/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?/,
+        exclude: 'node_modules',
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
